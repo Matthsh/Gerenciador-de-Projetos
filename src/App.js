cdefa8e4199b7688552bf5d24ccaeedb659c4c2b
Matthsh <mattheusmedina@outlook.com>
2022-09-17 16:03:18 -0300

    Criando componentes no React

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import HelloWorld from './components/HelloWorld'
 import './App.css';
 
 function App() {
+
+  const name = 'Mattheus'
+
+  const newName= name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+  
   return (
     <div className="App">
-      <h1>Ola React</h1>
-      <p>Meu primeiro App React s2</p>
+      <h2>Alterando o JSX</h2>
+      <p>Olá, {name}</p>    
+      <p>soma 2 + 2 = {2 + 2}</p>
+      <p>Olá, {newName}</p>
+      <p>soma 3 + 3 = {sum(3, 3)}</p>    
+      <img src={url} alt="minha imagem"/>
+      <HelloWorld />
     </div>
   );
 }
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import Frase from './Frase.js'
+
+function HelloWorld() {
+    return (
+        <div>
+            <Frase /><Frase /><Frase />
+            <h1>Meu pimeiro componente</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld
