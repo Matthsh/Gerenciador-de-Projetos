847a2eebc6f50571c416b440d3f5483680252860
Matthsh <mattheusmedina@outlook.com>
2022-09-18 18:51:16 -0300

    Trabalhando com props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import './App.css'
 import HelloWorld from './components/HelloWorld'
-import './App.css';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
 
+  /* AULA DE INTRODUÇÃO AO REACT
   const name = 'Mattheus'
 
   const newName= name.toUpperCase()
@@ -12,8 +15,12 @@ function App() {
   }
 
   const url = 'https://via.placeholder.com/150'
-  
+  */
+
+  const nome = 'Maria'
+
   return (
+    /* AULA DE INTRODUÇÃO AO REACT
     <div className="App">
       <h2>Alterando o JSX</h2>
       <p>Olá, {name}</p>    
@@ -23,7 +30,17 @@ function App() {
       <img src={url} alt="minha imagem"/>
       <HelloWorld />
     </div>
-  );
+    */
+
+   <div className="App">
+    <SayMyName nome="Mattheus" />
+    <SayMyName nome="João" />
+    <SayMyName nome={nome} />
+    <Pessoa foto="https://via.placeholder.com/150" nome="Mattheus" idade="27" profissao="Cientista" />
+   </div>
+    
+
+  )
 }
 
 export default App;
